add92a8dd4f8e4e7b1ec1a8e5beb62bcf397972f
Pense2 <jackschilling@live.com>
2023-01-24 18:34:45 -0600

    Bruh

--- Student_Names.py
+++ Student_Names.py
@@ -1,8 +1,16 @@
 s = open('studentnames.txt', 'r')
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
+num = 0
+# read = s.readline().rstrip('\n')
+for student in s:
+    name = student.rstrip('\n')
+    print(name)
+    num+=1
+# i-=1
+print("There were", num, "students in the file.")
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
 s.close()
